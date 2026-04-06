9aa74b1290ed8237b183b4cb6f65c46a9fcf7da6
sora0510 <nguyenhiep05102005@gmail.com>
2026-04-06 11:02:20 +0700

    Polish unit visuals and UI

--- resources/scout.gd
+++ resources/scout.gd
@@ -1,10 +1,17 @@
 extends Unit
 #platonic scout
 
+const HUMAN_TEXTURE := preload("res://assets/scout1.png")
+const AI_TEXTURE := preload("res://assets/scout2.png")
+
 func _ready():
 	display_name = "Scout"
 	mouse_filter = Control.MOUSE_FILTER_IGNORE
 	health = 7
+	team_textures = {
+		0: HUMAN_TEXTURE,
+		1: AI_TEXTURE,
+	}
 	
 	var move = preload("res://abilities/ability_move.gd").new()
 	move.move_range = 5
@@ -14,6 +21,10 @@ func _ready():
 	shoot.damage = 7
 	
 	abilities = [
-		move,
-		shoot
+		shoot,
+		move
 	]
+
+	super._ready()
+	if sprite != null:
+		sprite.scale = Vector2(0.18, 0.18)
